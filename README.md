782ab4946195e832121db4d776b7966ec70e7289
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-11-08 22:11:58 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # criacaochincilas
- Criação de Chinchilas
+**Criação de Chinchilas**  
+Digamos que o número de chinchilas de uma fazenda triplica a cada ano,
+após o primeiro ano. Elaborei um programa que ler o número inicial de
+chinchilas e anos e informe ano a ano o número médio previsto de chinchilas
+da fazenda. Validar a entrada para que o número inicial de chinchilas seja
+maior ou igual a 2 (um casal).
--- script.js
+++ script.js
@@ -4,29 +4,33 @@ function triplicarChinchilas() {
   let inAno = document.getElementById('inAno')
   let outResposta = document.getElementById('outResposta')
 
+  //Obtem conteúdo dos campos de entrada
   let animal = Number(inNumero.value)
   let ano = Number(inAno.value)
-
-
+  
+  //Declara variável que irá concatenar resultado
   let resultado = ''
 
-  if (animal <= 2 && animal == 0 ) {
+  //Valida a entrada (número inicial maior ou igual a 2)
+  if (animal < 2 || animal == 0 ) {
     alert('Digite um número correto (casal)')
     inNumero.onfocus()
     return
   }
+  //Cria um laço de repetição de 1 até o ano informado
+  for (let i = 1; i <= ano; i ++ ) {
+    if (i == 1) { //Se igual a 1, exibe a quantidade informada
+      resultado += i + '° ano: ' + animal + ' Chinchilas ' + '\n'
 
-  for (let animal = 1; animal <= ano; animal ++ ) {
-    if (animal == 1) {
-      resultado += animal + '° ano: ' + animal + '\n'
-
-  }else{
+  }else{ //Se não, multiplica a quantidade de chinchilas por 3
       animal = animal * 3
-      resultado += animal + '° ano: ' + animal + '\n'
+      resultado += i + '° ano: ' + animal + ' Chinchilas ' + '\n'
   }
 }
-  outResposta.textContent = resultado
+//Exibe o resultado
+outResposta.textContent = resultado  
 
 }
+//Cria referência ao botão e após associa function ao evento click
 let btMostrar = document.getElementById('btMostrar')
 btMostrar.addEventListener('click', triplicarChinchilas)
